2b6c9d55db576362ed637f6d4308369a142fff1a
David Godfrey <kinqvidda@gmail.com>
2024-04-10 14:16:58 +0100

    Created comment

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,6 @@
 fun main() {
+
+    //Array of random words
     val wordArray1 = arrayOf(
         "24/7",
         "multi-tier",
@@ -20,14 +22,17 @@ fun main() {
         "vision"
     )
 
+    //Get lengths of array
     val arraySize1 = wordArray1.size
     val arraySize2 = wordArray2.size
     val arraySize3 = wordArray3.size
 
+    //Random number generated
     val rand1 = (Math.random() * arraySize1).toInt()
     val rand2 = (Math.random() * arraySize2).toInt()
     val rand3 = (Math.random() * arraySize3).toInt()
 
+    //Putting it all together
     val phrase = "${wordArray1[rand1]} ${wordArray2[rand2]} ${wordArray3[rand3]}"
     println(phrase)
 }
